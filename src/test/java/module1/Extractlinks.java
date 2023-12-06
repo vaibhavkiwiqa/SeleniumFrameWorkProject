1bf4419ddeb12071c8ea9f5a3b29ab3340877047
kavita.kushwaha <kavita.kushwaha@kiwiqa.com>
2023-12-06 11:37:25 +0530

    New added Test cases

--- src/test/java/module1/Extractlinks.java
+++ src/test/java/module1/Extractlinks.java
@@ -1,12 +1,41 @@
 package module1;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import basePackage.BaseInit;
 
 public class Extractlinks extends BaseInit {
 	
-	public void testExtractlinks() 
+	
+	@BeforeTest
+	public static void setUp() throws Exception {
+
+		startUP();
+	}
+	
+	@Test
+	public void testExtractlinks() throws InterruptedException 
 	{
+		driver.get(prop.getProperty("url"));
+		Thread.sleep(2000);
+		WebElement main=driver.findElement(By.xpath("//*[@id=\"right_sidebar\"]/div[2]/div[2]"));
+		List<WebElement> list=main.findElements(By.tagName("a"));
+		System.out.println(list.size());
 		
+		for(int counter=0;counter<list.size();counter++) {
+			
+			
+			list.get(counter).click();
+			logs.info(driver.getTitle()+">>" +driver.getCurrentUrl());
+			driver.navigate().back();
+            main=driver.findElement(By.xpath("//*[@id=\"right_sidebar\"]/div[2]/div[2]"));
+		    list=driver.findElements(By.tagName("a"));
+		}
 		
 	}
 
