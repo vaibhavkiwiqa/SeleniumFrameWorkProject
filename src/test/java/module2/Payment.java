7187d5c88d2b3beab9c5ed1e19523b57ecb3be2c
kavita.kushwaha <kavita.kushwaha@kiwiqa.com>
2023-12-18 15:22:40 +0530

    New class added

--- src/test/java/module2/Payment.java
+++ src/test/java/module2/Payment.java
@@ -1,11 +1,35 @@
 package module2;
 
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import basePackage.BaseInit;
+import utility.MyLibrary;
 
 public class Payment extends BaseInit{
+	
+	@BeforeTest
+	public static void setUp() throws Exception {
 
-	public void testPayment() 
+		startUP();
+
+	}
+  @Test
+	public void testPayment() throws InterruptedException 
 	{
+		MyLibrary.signIn();
+		SearchProduct.testSearchProduct();  
+		Thread.sleep(1000);
+		isElementPresent("lnk_checkout_linkText").click();
+		driver.findElement(By.name("comments")).sendKeys("New product is added");
+		isElementPresent("btn_continue_id").click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/form/div/div[4]/table[1]/tbody/tr/td[2]/input")).click();
+		driver.findElement(By.name("comments")).sendKeys("Product is confirmed");
+		isElementPresent("btn_continue_id").click();
+		Thread.sleep(2000);
+		isElementPresent("btn_confirmorder_id").click();
 		
 		
 	}
--- src/test/java/module2/SearchProduct.java
+++ src/test/java/module2/SearchProduct.java
@@ -1,6 +1,10 @@
 package module2;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -16,9 +20,28 @@ public class SearchProduct extends BaseInit {
 	@Test
 	public static void testSearchProduct() {
 		driver.get(prop.getProperty("url"));
-		isElementPresent("ip_search_name").sendKeys("JBL T450");
+		isElementPresent("ip_search_name").sendKeys("JBL");
 		isElementPresent("ip_search_name").sendKeys(Keys.ENTER);
-		logs.info(driver.getTitle());
+		//logs.info(driver.getTitle());
+		
+		WebElement mainsearch=driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div"));
+		List<WebElement> searchproduct=mainsearch.findElements(By.tagName("a"));
+		
+		for(int counter=0;counter<searchproduct.size();counter++) {
+			
+			String productname=searchproduct.get(counter).getText();
+			
+			if(productname.equalsIgnoreCase("JBL T250SI")) {
+				
+				isElementPresent("lnk_product_linkText").click();
+				isElementPresent("btn_addtocart_id").click();
+			}
+			else
+			{
+				
+				System.out.println("No product found");
+			}
+		}
 		
 		
 		
